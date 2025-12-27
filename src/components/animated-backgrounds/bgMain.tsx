cd43436ce7bf054b54d4f360abfaf04e8b6c6703
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2025-12-28 00:02:33 +0530

    fix: new ui dashboard

--- src/components/animated-backgrounds/bgMain.tsx
+++ src/components/animated-backgrounds/bgMain.tsx
@@ -3,15 +3,16 @@ import Image from "next/image"
 const BgMain = () => {
   return (
     <div
-    className="absolute top-50 inset-0 -z-10 blur-3xl"
+    className="absolute top-45 inset-0 -z-10 blur-3xl"
     >
     <Image
         alt=""
         width={1000}
         height={1000}
-        src="/1.jpg"
-        className="absolute h-screen w-screen md:w-[calc(100vw-8rem)] -translate-x-1/2 left-1/2 duration-200 ease-linear"
+        src="/3.jpg"
+        className="absolute h-screen w-screen md:w-[calc(100vw-8rem)] -translate-x-1/2 left-1/2 opacity-70 mix-blend-screen"
     />
+     <div className="absolute inset-0 bg-white/20 dark:bg-black/20" />
     </div>
   )
 }
