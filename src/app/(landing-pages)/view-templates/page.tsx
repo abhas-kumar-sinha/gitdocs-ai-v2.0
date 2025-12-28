3c65d4c002e4af05351563478bd6c54f44c6b318
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2025-12-28 20:55:11 +0530

    feat: templates page

--- src/app/(landing-pages)/view-templates/page.tsx
+++ src/app/(landing-pages)/view-templates/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
-import { CircleCheckBig } from "lucide-react";
+import TemplateList from "@/components/template/TemplateList";
+import { templates } from "@/lib/constants/CONSTANTS";
 
 const LandingPage = () => {
   return (
@@ -21,19 +22,18 @@ const LandingPage = () => {
         <div className="w-9/10 bg-sidebar min-h-screen mt-40 mb-20 flex flex-col rounded-3xl p-8 text-sidebar-foreground">
           <div className="flex items-start justify-between">
             <div>
-              <h2 className="text-xl">Gitdocs Release History</h2>
-              <p className="text-sidebar-foreground/50">Complete release history with detailed change tracking</p>
-            </div>
-            <div className="flex items-center gap-x-2 text-xs py-1 px-2 rounded-md bg-primary/20 text-primary font-thin font-mono">
-              <CircleCheckBig size={10} />
-              2.0.0
+              <h2 className="text-xl">All Templates</h2>
+              <p className="text-sidebar-foreground/50">Browse ready-to-use templates for every workflow</p>
             </div>
           </div>
 
-          <hr className="mt-6 mb-10 border-border" />
+          <hr className="my-6 border-border" />
 
+          <div className="grid md:px-20 lg:px-0 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mt-3">
+            <TemplateList templates={templates} />
+          </div>
 
-          <hr className="my-6 border-border mt-auto" />
+          <hr className="my-6 border-border" />
 
           <div className="flex flex-col md:flex-row gap-y-6 item-center justify-between md:px-8 py-2">
             <div className="flex flex-col items-start">
